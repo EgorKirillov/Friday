cd1e64534e241da9035957d7ee62c576c4090c8b
EgorKirillov <80296253508@mail.ru>
2022-08-20 17:40:27 +0300

    change HashRouter to BrowserRouter

--- src/features/mainPage/UI/StartPage.tsx
+++ src/features/mainPage/UI/StartPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import DevHeader from '../../../common/components/DevHeader/DevHeader'
 import SwitchRoutes from '../../../common/components/Routing/SwitchRoutes'
 import { Header } from '../../../common/components/Header/Header'
