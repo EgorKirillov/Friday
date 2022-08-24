bc92feabfedf3a796514596f9fa8cd09c9c4a6f4
EgorKirillov <80296253508@mail.ru>
2022-08-24 21:21:50 +0300

    refactoring remove Provider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import './App.css'
 import { StartPage } from '../features/mainPage/UI/StartPage'
-import { Provider } from 'react-redux'
-import { store } from './store'
 
 function App() {
   return (
     <div className="App">
-      <Provider store={store}>
-        <StartPage />
-      </Provider>
+      <StartPage />
     </div>
   )
 }
