4ba1195d65342d7d810a8ec6bdabfd33a4351830
Belom7 <Still07pkm@gmail.com>
2022-08-26 02:11:51 +0300

    реализовал логику: 1)если приложение не проинициализированно то мы показываем крутилку, только потом отображаем всю страницу 2) если идут какие то запросы на сервер показывается лоадер ( бегущая строка) которая дает нам понять что идет загрузка

--- src/features/mainPage/UI/StartPage.tsx
+++ src/features/mainPage/UI/StartPage.tsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+import { CircularProgress } from '@mui/material'
+import LinearProgress from '@mui/material/LinearProgress'
 
 import DevHeader from '../../../common/components/DevHeader/DevHeader'
 import { Header } from '../../../common/components/Header/Header'
 import SwitchRoutes from '../../../common/components/Routing/SwitchRoutes'
+import { useAppDispatch, useAppSelector } from '../../../common/hooks/hooks'
+
+import { initializeTC } from './startPage-reducer'
 
 export function StartPage() {
+  const status = useAppSelector(state => state.startPage.status)
+  const dispatch = useAppDispatch()
+  const isInitialize = useAppSelector(state => state.startPage.isInitialize)
+
+  useEffect(() => {
+    dispatch(initializeTC())
+  }, [])
+
+  if (!isInitialize) {
+    return (
+      <div style={{ position: 'fixed', top: '30%', textAlign: 'center', width: '100%' }}>
+        <CircularProgress />
+      </div>
+    )
+  }
+
   return (
     <div>
       <DevHeader />
       <Header />
+      {status === 'loading' && <LinearProgress color="inherit" />}
       <SwitchRoutes />
     </div>
   )
