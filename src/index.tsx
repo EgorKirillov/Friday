4c8d5699edfcc6b7e57070f49641c47da6e938fc
EgorKirillov <80296253508@mail.ru>
2022-08-26 19:55:01 +0300

    hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 import App from './app/App'
 import { store } from './app/store'
@@ -12,11 +12,11 @@ import './index.css'
 const root = createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 )
 
 // If you want to start measuring performance in your app, pass a function
