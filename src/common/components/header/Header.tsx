0b1d24f7c7cf638e12f0f9119b146c609d5d34a5
Belom7 <Still07pkm@gmail.com>
2022-09-01 00:47:38 +0300

    рефактор имен

--- src/common/components/header/Header.tsx
+++ src/common/components/header/Header.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 import logo from '../../../assets/img/itIncubatorLOGO.png'
 
-import { BlockAvaName } from './aaa/BlockAvaName'
+import { BlockBasicMenu } from './blockBasicMenu/BlockBasicMenu'
 import s from './Header.module.css'
 
 export function Header() {
   return (
     <div className={s.headerContainer}>
       <img className={s.logo} src={logo} alt="" />
-      <BlockAvaName />
+      <BlockBasicMenu />
     </div>
   )
 }
