320b2b2f7d8f437918ca0ad2c6b84130daf0a198
Belom7 <Still07pkm@gmail.com>
2022-09-02 15:14:32 +0300

    всплывашка в моем паке

--- src/features/packs/UI/titleBlock/TitleBlock.tsx
+++ src/features/packs/UI/titleBlock/TitleBlock.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
+import PendingIcon from '@mui/icons-material/Pending'
+
+import deleteIcon from '../../../../assets/svg/Delete.svg'
+import editIcon from '../../../../assets/svg/Edit.svg'
+import learnIcon from '../../../../assets/svg/teacher.svg'
+import { BasicMenu } from '../../../../common/components/basicMenu/BasicMenu'
+import { ButtonBasicMenu } from '../../../../common/components/basicMenu/buttonProfile/ButtonBasicMenu'
 import { ButtonWithLoader } from '../../../../common/components/buttonWithLoader/ButtonWithLoader'
-import { useAppSelector } from '../../../../common/hooks/hooks'
+import { useAppDispatch, useAppSelector } from '../../../../common/hooks/hooks'
 
 import s from './TitleBlock.module.css'
 
@@ -9,6 +16,7 @@ type PropsType = {
   title: string
   buttonName?: string
   buttonCallback?: () => void
+  isMyPack?: boolean
   buttonVisability?: 'hidden' | 'visible'
   callbackTitle?: () => void
 }
@@ -17,17 +25,47 @@ export const TitleBlock = ({
   title,
   buttonName,
   buttonCallback,
+  isMyPack,
   buttonVisability = 'visible',
   callbackTitle,
 }: PropsType) => {
   const loading = useAppSelector(state => state.app.status)
   const isLoading = loading === 'loading'
 
+  const dispatch = useAppDispatch()
+
+  const onClickHandlerEdit = () => {
+    alert('Edit!')
+  }
+  const onClickHandlerDelete = () => {
+    alert('Delete!')
+  }
+  const onClickHandlerLearn = () => {
+    alert('Learn!')
+  }
+
+  const buttonEdit = <ButtonBasicMenu icon={editIcon} name="Edit" callBack={onClickHandlerEdit} />
+  const buttonDelete = (
+    <ButtonBasicMenu icon={deleteIcon} name="Delete" callBack={onClickHandlerDelete} />
+  )
+  const buttonLearn = (
+    <ButtonBasicMenu icon={learnIcon} name="Learn" callBack={onClickHandlerLearn} />
+  )
+
+  const arrButton = [buttonEdit, buttonDelete, buttonLearn]
+
   return (
     <div>
       <div className={s.title}>
-        <div>
+        <div style={{ display: 'flex' }}>
           <h2 onClick={callbackTitle}>{title}</h2>
+          <div style={{ lineHeight: '68px' }}>
+            <BasicMenu
+              label={isMyPack && <PendingIcon />}
+              items={arrButton}
+              style={{ margin: '8px 0 0 -50px' }}
+            />
+          </div>
         </div>
 
         <ButtonWithLoader
