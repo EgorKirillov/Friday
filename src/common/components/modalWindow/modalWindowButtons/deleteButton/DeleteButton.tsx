1d76db70587348cb7d7ade2eaa81c74805d7efe4
Belom7 <Still07pkm@gmail.com>
2022-09-03 05:02:59 +0300

    кнопка delete

---
+++ src/common/components/modalWindow/modalWindowButtons/deleteButton/DeleteButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import style from './DeleteButton.module.css'
+
+export const DeleteButton = () => {
+  return (
+    <>
+      <button className={style.deleteButton}>Delete</button>
+    </>
+  )
+}
