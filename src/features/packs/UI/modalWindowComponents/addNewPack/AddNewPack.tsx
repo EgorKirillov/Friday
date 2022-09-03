751b0560a1876f632b83819ac72ad65f2b0cc267
Belom7 <Still07pkm@gmail.com>
2022-09-03 05:07:28 +0300

    карта добавления нового пака

---
+++ src/features/packs/UI/modalWindowComponents/addNewPack/AddNewPack.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+import { CancelButton } from '../../../../../common/components/modalWindow/modalWindowButtons/cancelButton/CancelButton'
+import { SaveButton } from '../../../../../common/components/modalWindow/modalWindowButtons/saveButton/SaveButton'
+import { ModalCheckBox } from '../../../../../common/components/modalWindow/modalWindowChackBox/modalWindowCheckBox'
+import { ModalWindowInput } from '../../../../../common/components/modalWindow/modalWindowInput/ModalWindowInput'
+import style from '../../../../../common/components/modalWindow/style/StylePacks.module.css'
+
+export const AddNewPack = () => {
+  const [name, setName] = useState('')
+  const [checked, setChecked] = useState(false)
+
+  return (
+    <div>
+      <ModalWindowInput name={name} setName={setName} />
+      <div className={style.checkBox}>
+        <ModalCheckBox checked={checked} setChecked={setChecked} />
+      </div>
+      <div className={style.buttonsBlock}>
+        <CancelButton />
+        <SaveButton />
+      </div>
+    </div>
+  )
+}
