6c74282914d6b62a785ed90924a70de7a35d1d35
Belom7 <Still07pkm@gmail.com>
2022-09-03 05:01:34 +0300

    кнопка cancel

---
+++ src/common/components/modalWindow/modalWindowButtons/cancelButton/CancelButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import style from './CancelButton.module.css'
+
+export const CancelButton = () => {
+  return (
+    <>
+      <button className={style.cancelButton}>Cancel</button>
+    </>
+  )
+}
