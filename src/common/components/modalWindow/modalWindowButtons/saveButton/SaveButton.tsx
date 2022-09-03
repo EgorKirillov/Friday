b4d39e1e896087d3cb0598e7761ac79eae5a2260
Belom7 <Still07pkm@gmail.com>
2022-09-03 05:03:15 +0300

    кнопка save

---
+++ src/common/components/modalWindow/modalWindowButtons/saveButton/SaveButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import style from './SaveButton.module.css'
+
+export const SaveButton = () => {
+  return (
+    <>
+      <button className={style.saveButton}>Save</button>
+    </>
+  )
+}
