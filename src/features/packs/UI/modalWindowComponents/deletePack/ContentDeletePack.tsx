0f2411c7376d811464ffb00dd56e85ada82595d6
Belom7 <Still07pkm@gmail.com>
2022-09-04 22:40:02 +0300

    контент для компоненты удаления пачки (имя пачки и кнопки)

--- src/features/packs/UI/modalWindowComponents/deletePack/ContentDeletePack.tsx
+++ src/features/packs/UI/modalWindowComponents/deletePack/ContentDeletePack.tsx
@@ -1,21 +1,39 @@
 import React from 'react'
 
-import { CancelButton } from '../../../../../common/components/modalWindow/modalWindowButtons/cancelButton/CancelButton'
-import { DeleteButton } from '../../../../../common/components/modalWindow/modalWindowButtons/deleteButton/DeleteButton'
+import { ButtonWithLoader } from '../../../../../common/components/buttonWithLoader/ButtonWithLoader'
 import style from '../../../../../common/components/modalWindow/style/StylePacks.module.css'
+import { useAppSelector } from '../../../../../common/hooks/hooks'
 
-export const ContentDeletePack = () => {
+type PropsType = {
+  close: () => void
+  callBack?: () => void
+  name: string
+}
+
+export const ContentDeletePack = (props: PropsType) => {
+  const status = useAppSelector(state => state.app.status)
+
   return (
     <div>
       <div className={style.content}>
         <p>
-          Do you really want to remove <b>Pack Name</b>?
+          Do you really want to remove <b>{props.name}</b>?
         </p>
         <p>All cards will be deleted.</p>
       </div>
       <div className={style.buttonsBlock}>
-        {/*<CancelButton />*/}
-        <DeleteButton />
+        <ButtonWithLoader
+          name={'Cancel'}
+          styleButton={'cancel'}
+          onClick={props.close}
+          isLoading={status === 'loading'}
+        />
+        <ButtonWithLoader
+          name={'Delete'}
+          styleButton={'delete'}
+          onClick={props.callBack}
+          isLoading={status === 'loading'}
+        />
       </div>
     </div>
   )
