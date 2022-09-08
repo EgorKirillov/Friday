a2a1ad2e5e138c918a8a9dfb170047f039b7091d
Belom7 <Still07pkm@gmail.com>
2022-09-08 14:42:07 +0300

    refactor component

--- src/features/cards/UI/modalWindowComponents/deleteCard/DeleteCard.tsx
+++ src/features/cards/UI/modalWindowComponents/deleteCard/DeleteCard.tsx
@@ -2,29 +2,38 @@ import React from 'react'
 
 import { ModalWindow } from '../../../../../common/components/modalWindow/ModalWindow'
 import { useAppDispatch, useAppSelector } from '../../../../../common/hooks/hooks'
-import { changeCardModalStatus, deleteCard } from '../../../cardReducer'
+import { changeCardModalStatus, deleteCard, setQuestionCard, setIdCard } from '../../../cardReducer'
 
 import { ContentDeleteCard } from './ContentDeleteCard'
 
 type PropsType = {
   callBack?: () => void
-  name?: string
-  idCard: string
+  cardName: string
 }
 
 export const DeleteCard = (props: PropsType) => {
   const open = useAppSelector(state => state.cards.modalDelete)
-
   const dispatch = useAppDispatch()
+  const idCard = useAppSelector(state => state.cards.tempIdCard)
 
-  const closeModal = () => dispatch(changeCardModalStatus('modalDelete', false))
+  const closeModal = () => {
+    dispatch(changeCardModalStatus('modalDelete', false))
+    dispatch(setIdCard(''))
+    dispatch(setQuestionCard(''))
+  }
 
-  const deleteCardHandler = () => dispatch(deleteCard(props.idCard))
+  const deleteCardHandler = () => {
+    dispatch(deleteCard(idCard))
+  }
 
   return (
     <div>
       <ModalWindow title={'Delete Card'} open={!!open} onClose={closeModal}>
-        <ContentDeleteCard onClose={closeModal} callBack={deleteCardHandler} name={props.name} />
+        <ContentDeleteCard
+          onClose={closeModal}
+          callBack={deleteCardHandler}
+          name={props.cardName}
+        />
       </ModalWindow>
     </div>
   )
